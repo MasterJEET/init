92dc941cf9654e67bad7d4e946eec3b7d8bc1cde
Jitendra Marndi <jitendramarndi@gmail.com>
2021-03-03 19:54:58 -0700

    Added git completion and git prompt support

--- jmbashrc.sh
+++ jmbashrc.sh
@@ -7,6 +7,14 @@ bashrc=~/.bashrc
 header=">>> JM_BASH_CUSTOMIZATOIN | START >>>"
 footer="<<< JM_BASH_CUSTOMIZATOIN | END <<<"
 
+git_completion_url="https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash"
+git_prompt_url="https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh"
+
+git_completion_file="$(basename $git_completion_url)"
+git_prompt_file="$(basename $git_prompt_url)"
+
+git_dir="$(dirname $0)/git"
+
 ### CUSTOMIZATION TO BE ADDED
 content="
 # $header
@@ -22,11 +30,36 @@ parse_git_branch() {
 export PS1='\[\e[36m\][\[\e[m\]\[\e[31m\]\u\[\e[m\]\[\e[33m\]@\[\e[m\]\[\e[32m\]\h\[\e[m\]:\[\e[36m\]\W]\[\e[m\]\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ '
 export WORKON_HOME=~/.virtualenvs
 
+# git completion
+if [[ -f $git_dir/$git_completion_file ]]; then
+    source $git_dir/$git_completion_file
+fi
+
+# git prompt
+if [[ -f $git_dir/$git_prompt_file ]]; then
+    source $git_dir/$git_prompt_file
+fi
+
 # $footer
 "
 
+function download_git_files {
+
+    mkdir -p $git_dir
+
+    if [[ ! -f "$git_dir/$git_completion_file" ]]; then
+        curl -o $git_dir/$git_completion_file $git_completion_url
+    fi
+
+    if [[ ! -f "$git_dir/$git_prompt_file" ]]; then
+        curl -o $git_dir/$git_prompt_file $git_prompt_url 
+    fi
+}
+
 
 #### MAIN SECTION ####
+
+download_git_files
 
 if [[ ! -f $bashrc ]]; then
     echo "$bashrc file not found, creating it ..."
